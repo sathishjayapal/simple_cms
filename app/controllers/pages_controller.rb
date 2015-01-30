7bd9c8b414d6739e6e50aa422f20837c4b070854
Sathish Jayapal <sjayapal@skminfotech.com>
2015-01-30 05:43:49 -0600

    More crud changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,55 @@
 class PagesController < ApplicationController
+  layout=false
   def index
     @pages= Page.all
   end
 
   def show
+    @page=Page.find(params[:id])
   end
 
   def new
+    @page=Page.find(params[:id])
   end
 
   def create
+    if params[:id].nil? 
+      render('new')
+    else
+      @page=Page.save(pages_params)
+      if @page.save
+        flash[:notice]="Page saved succesfully"
+        redirect_to(:action=>index)
+      else
+        render new
+      end
+    end
   end
 
   def edit
+    @page=Page.find(params[:id])
   end
 
   def update
+    # @page=Page.find(params[:id]
+    #   if @page.update_attributes(pages_params)
+    #     flash[:notice]="Page updated succesfully"
+    #     redirect_to(:action=>'index')
+    #     else
+    #       render(edit)
+    #     end
   end
 
   def delete
+    @page=Page.find(params[:id])
   end
-
   def destroy
+    page=Page.find(params[:id])
+    page.destroy
+    flash[:notice]= "page '#{page.name}' destroyed succesfully"
+     redirect_to(:action=>'index')
+  end
+  def pages_params
+    params.require(:page).permit(:subject_id,:name,:permalink,:position,:visible)
   end
 end
--- app/controllers/section_controller.rb
+++ app/controllers/section_controller.rb
@@ -1,10 +1,11 @@
 class SectionController < ApplicationController
   layout false
   def index
-    @sections=Section.all
+    @sections=Section.sorted
   end
 
   def show
+
      @section = Section.find(params[:id])
   end
 
@@ -28,14 +29,31 @@ class SectionController < ApplicationController
   end
 
   def edit
+    
+    @section=Section.find(params[:id])
   end
 
   def update
+    @section=Section.find(params[:id])
+    if(@section.update_attributes(section_params))
+      flash[:notice]="Section updated succesfully"
+      redirect_to(:action=>'index')
+      else
+        render(edit)
+      end
   end
 
   def delete
+    @section=Section.find(params[:id])
   end
 
   def destroy
+    section=Section.find(params[:id])
+     section.destroy
+     flash[:notice]= "Section '#{section.name}' destroyed succesfully"
+     redirect_to(:action=>'index')
+  end
+  def section_params
+    params.require(:section).permit(:position,:name,:visible,:page_id,:content,:content_type)
   end
 end
